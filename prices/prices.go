053a1d7263c139dadf822382e75379c605ee9855
Yuvraj Shekhar <syuvraj1999@gmail.com>
2025-03-11 17:11:16 +0100

    added taxes into external json file

--- prices/prices.go
+++ prices/prices.go
@@ -1,11 +1,40 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrice        []float64
 	TaxIncludedPrices map[string]float64
+}
+
+func (job TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("prices.txt")
+
+	if err != nil {
+		fmt.Println("Could not open file!")
+		fmt.Println(err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+
+	if err != nil {
+		fmt.Println("Reaading the file content failed")
+		fmt.Println("err")
+		return
+	}
 }
 
 func (job TaxIncludedPriceJob) Process() {
